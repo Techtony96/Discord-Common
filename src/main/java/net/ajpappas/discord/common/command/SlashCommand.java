103b26b13582f3a85131fdd1423901d02b4227dc
Tony Pappas <techtony96@gmail.com>
2023-03-09 18:41:38 -0700

    Add permissions to SlashCommand.java

--- src/main/java/net/ajpappas/discord/common/command/SlashCommand.java
+++ src/main/java/net/ajpappas/discord/common/command/SlashCommand.java
@@ -1,6 +1,7 @@
 package net.ajpappas.discord.common.command;
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.rest.util.PermissionSet;
 import reactor.core.publisher.Mono;
 
 public interface SlashCommand {
@@ -8,4 +9,8 @@ public interface SlashCommand {
     String getName();
 
     Mono<Void> handle(ChatInputInteractionEvent event);
+
+    default PermissionSet requiredPermissions() {
+        return PermissionSet.none();
+    }
 }
--- src/main/java/net/ajpappas/discord/common/listeners/SlashCommandListener.java
+++ src/main/java/net/ajpappas/discord/common/listeners/SlashCommandListener.java
@@ -2,6 +2,9 @@ package net.ajpappas.discord.common.listeners;
 
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.entity.Member;
+import discord4j.rest.util.Permission;
+import discord4j.rest.util.PermissionSet;
 import net.ajpappas.discord.common.command.SlashCommand;
 import net.ajpappas.discord.common.util.ErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +29,34 @@ public class SlashCommandListener {
 
     public Mono<Void> handle(ChatInputInteractionEvent event) {
         return Flux.fromIterable(commands)
-            .filter(command -> command.getName().equals(event.getCommandName()))
-            .next()
-            .flatMap(command -> command.handle(event)
-                    .onErrorResume(error -> ErrorHandler.handleError(error, msg -> event.reply(msg).withEphemeral(true)))
-            );
+                .filter(command -> command.getName().equals(event.getCommandName()))
+                .next()
+                .flatMap(command -> {
+                            if (!hasPermission(command, event))
+                                return event.reply("You do not have permission for this command.").withEphemeral(true);
+                            return command.handle(event)
+                                    .onErrorResume(error -> ErrorHandler.handleError(error, msg -> event.reply(msg).withEphemeral(true)));
+                        }
+                );
+    }
+
+    private boolean hasPermission(SlashCommand command, ChatInputInteractionEvent event) {
+        if (command.requiredPermissions() == null || command.requiredPermissions().isEmpty())
+            return true;
+
+        // If this command wasn't executed in a guild, we can't check permissions
+        if (Boolean.FALSE.equals(event.getInteraction().getGuild().hasElement().block())) {
+            return false;
+        }
+
+        PermissionSet memberPermissions = event.getInteraction().getMember().map(Member::getBasePermissions).get().block();
+
+        for (Permission permission : command.requiredPermissions()) {
+            if (!memberPermissions.contains(permission))
+                return false;
+        }
+
+        // User has all required permissions
+        return true;
     }
 }
