c65bec539103f9133d743b350a9d439c8db5e99f
Tony Pappas <techtony96@gmail.com>
2023-03-18 09:24:14 -0600

    Fix restclient

--- src/main/java/net/ajpappas/discord/common/service/GlobalCommandRegistrar.java
+++ src/main/java/net/ajpappas/discord/common/service/GlobalCommandRegistrar.java
@@ -1,11 +1,13 @@
 package net.ajpappas.discord.common.service;
 
 import discord4j.common.JacksonResources;
+import discord4j.core.GatewayDiscordClient;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import discord4j.rest.RestClient;
 import discord4j.rest.service.ApplicationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.Resource;
@@ -19,13 +21,14 @@ import java.util.stream.Collectors;
 
 @Service
 public class GlobalCommandRegistrar implements ApplicationRunner {
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    private final RestClient client;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    //Use the rest client provided by our Bean
-    public GlobalCommandRegistrar(RestClient client) {
-        this.client = client;
+    private final RestClient restClient;
+
+    @Autowired
+    public GlobalCommandRegistrar(GatewayDiscordClient client) {
+        this.restClient = client.getRestClient();
     }
 
     //This method will run only once on each start up and is automatically called with Spring so blocking is okay.
@@ -36,8 +39,8 @@ public class GlobalCommandRegistrar implements ApplicationRunner {
 
         // Convenience variables for the sake of easier to read code below.
         PathMatchingResourcePatternResolver matcher = new PathMatchingResourcePatternResolver();
-        final ApplicationService applicationService = client.getApplicationService();
-        final long applicationId = client.getApplicationId().block();
+        final ApplicationService applicationService = restClient.getApplicationService();
+        final long applicationId = restClient.getApplicationId().block();
 
         //Get our commands json from resources as command data
         List<ApplicationCommandRequest> commands = new ArrayList<>();
@@ -51,10 +54,10 @@ public class GlobalCommandRegistrar implements ApplicationRunner {
         /* Bulk overwrite commands. This is now idempotent, so it is safe to use this even when only 1 command
         is changed/added/removed
         */
-        LOGGER.info("Registering {} commands: {}", commands.size(), commands.stream().map(ApplicationCommandRequest::name).collect(Collectors.joining(",")));
+        log.info("Registering {} commands: {}", commands.size(), commands.stream().map(ApplicationCommandRequest::name).collect(Collectors.joining(",")));
         applicationService.bulkOverwriteGlobalApplicationCommand(applicationId, commands)
-                .doOnNext(command -> LOGGER.debug("Successfully registered {}", command.name()))
-                .doOnError(e -> LOGGER.error("Failed to register global commands", e))
+                .doOnNext(command -> log.debug("Successfully registered {}", command.name()))
+                .doOnError(e -> log.error("Failed to register global commands", e))
                 .subscribe();
     }
 }
